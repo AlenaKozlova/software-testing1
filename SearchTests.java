f049cb7f9ae84f4493e4a145fd3d5b12cba27428
Alena <43928596+AlenaKozlova@users.noreply.github.com>
2018-11-05 16:40:21 +0300

    Create SearchTests.java

---
+++ SearchTests.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import lib.CoreTestCase;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class SearchTests extends CoreTestCase
+{
+    @Test
+    public void testSearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForSearchResult("Object-oriented programming language");
+    }
+
+    @Test
+    public void testCancelSearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.waitForCancelButtonToAppear();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForCancelButtonToDisappear();
+    }
+}
